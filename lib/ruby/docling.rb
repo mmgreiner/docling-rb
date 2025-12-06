c6f005879d4e6a34786d0a229cbd0a88274f22ed
mmgreiner <mmgreiner@bluewin.ch>
2025-12-06 15:06:15 +0100

    init

---
+++ lib/ruby/docling.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "docling/version"
+
+module Ruby
+  module Docling
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/ruby/docling/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Ruby
+  module Docling
+    VERSION = "0.1.0"
+  end
+end
